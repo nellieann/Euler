f226f6fdace8aaeb83736c5372b5507d79c4c27e
Nellie Ann McCool <namccool@gmail.com>
2014-05-24 22:45:22 -0700

    begin problem 3, make test first

--- test/index.js
+++ test/index.js
@@ -10,3 +10,19 @@ describe("Problem 1", function(){
     problem1loop(1000).should.equal(answer);
   })
 })
+
+describe("Problem 2", function(){
+  var problem2 = require("../problem2/complete.js");
+  var answer = 4613732;
+  it("should work", function(){
+    problem2().should.equal(answer);
+  })
+})
+
+describe("Problem 3", function(){
+  var problem3 = require("../problem3/incomplete.js");
+  var answer = 6857;
+  it("should work", function(){
+    problem3(600851475143).should.equal(answer);
+  })
+})
