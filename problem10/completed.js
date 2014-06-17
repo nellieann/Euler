b82a2f61aada611b11b72921e77ec976eb24c773
Nellie Ann McCool <namccool@gmail.com>
2014-06-16 22:36:36 -0700

    complete problem 9 & 10

---
+++ problem10/completed.js
@@ -0,0 +1,26 @@
+/*
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+*/
+
+function isPrime(num) {
+  if (num < 2) return false;
+  if (num === 2) return true;
+  for (var i = 2; i < Math.sqrt(num); i++) {
+    if (num%i==0) return false;
+  }
+  return true;
+}
+
+function sumOfPrimes(){
+  var sum = 0;
+  for (var i = 2; i < 2000000; i++){
+    if (isPrime(i)){ sum += i; }
+   }
+  return sum;
+}
+
+console.log(sumOfPrimes());
