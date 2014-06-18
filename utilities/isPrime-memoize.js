1dd73e68f7f263a7f6b7fa96cace0e6ee6ccbe25
Nellie Ann McCool <namccool@gmail.com>
2014-06-17 23:52:27 -0700

    utility functions for isPrime

---
+++ utilities/isPrime-memoize.js
@@ -0,0 +1,18 @@
+var primeM = [];
+
+function isPrime(num){
+  var result = true;
+
+  if (typeof primeM[num] !== "undefined"){
+    return primeM[num];
+  }
+
+  if (num < 2){ result = false; }
+
+  for (var i = 2; i < num; i++){
+    if (num%i==0) result = false;
+  }
+
+  primeM[num] = result;
+  return result;
+}
